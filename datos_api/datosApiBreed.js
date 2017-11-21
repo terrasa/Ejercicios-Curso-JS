d2a1555c646f5bb2765d33f1a4fc22d8b72cd202
Luis Terrasa <terrasa.luis@gmail.com>
2017-11-21 15:56:03 +0100

    Pendiente solucionar next foto

--- datos_api/datosApiBreed.js
+++ datos_api/datosApiBreed.js
@@ -2,138 +2,81 @@
 
 //__  /api/breeds/list/all  Listado de razas y subrazas
 
-/*async function inserData(url){
-	let breed = document.getElementsByClassName("breed");
-    let datos = await fetch(url);
-    let datosJson = await datos.json(); // text.message
-		
-	for (let prop in datosJson.message) {
-		let breedP = document.createElement('p');
-		breed[0].appendChild(breedP);
-		//console.log(`datosJson.message.${prop} = ${datosJson.message[prop]}`);
-		breedP.addEventListener("click", function(){
-	 console.log('textCookies');
-		});
-		breedP.innerHTML = `${prop}`; // Comillas invertidas ``
-		
-		
-	}
-	//console.log('Propiedades: cada Raza: ', Object.keys(mensaje) );
-	//console.log('Propiedades: Raza index 5: ', Object.keys(mensaje)[5] );
-}*/
- 
- //  inserData('https://dog.ceo/api/breeds/list/all');
+let next = document.getElementById('next');
 
-/*async function llamaYPinta(url){
-    const primera = await fetch(url);
-    const text = await primera.json();
-	const mensaje = text.message;
-	console.log('text.message: ', text.message);
-	console.log('text.message.bulldog; ', text.message.bulldog); //Array con 2 elementos
-	console.log('text.message.bulldog[1]: ', text.message.bulldog[1]);
-	console.log('text.status: ', text.status);
-	console.log('text: ', text);
-	
-	mensaje.bulldog.forEach(function(element){
-		console.log('forEach text.message.bulldog: ', element);	
-	})
-	mensaje.bulldog.forEach(function(element, index){
-		console.log('forEach text.message.bulldog Index: ' , index);		
-	})
-	console.log('Propiedades: cada Raza: ', Object.keys(mensaje) );
-	console.log('Propiedades: Raza index 5: ', Object.keys(mensaje)[5] );
- }
- 
- llamaYPinta('https://dog.ceo/api/breeds/list/all');*/
-
-/*async function inserData(url, breed_div, objeto){
-	let breed = document.getElementsByClassName(breed_div);
+async function dataImg(url, breedImage){
     let data = await fetch(url);
     let dataJson = await data.json();
 	let result = dataJson.message;
-	console.lo(url);
-	console.log(breed_div);
-	console.log(objeto);
-	
-	console.log('Propiedades: cada Raza: ', Object.keys(result) );
-	console.log('Propiedades: Raza index 5: ', Object.keys(result)[5] );
-	
-	for(let prop = 0; prop < objeto.length; prop++){//Object.keys
-		let breedP = document.createElement('p');
-		breed[0].appendChild(breedP);
-		if(breed_div == 'breed'){
-			breedP.innerHTML = Object.keys(result)[prop]; // Comillas invertidas ``
+    console.log('result: ', result);
+	breedImage.src = result; 
+}	
+
+async function dataElement(breed_div, pos, prop){
+	let breed = document.getElementsByClassName(breed_div);
+	while(breed[0].childNodes.length > 3){
+			breed[0].removeChild(breed[0].childNodes[3]);
+		}
+	let raza = document.getElementById('raza');
+	let breedImage = document.getElementById('imageDog');
+    if(pos.length == 0){
+        raza.innerHTML =`La raza seleccionada es: ${prop} y no tiene subraza.`;
+		dataImg(`https://dog.ceo/api/breed/${prop}/ /images/random`, breedImage)
+    }
+	else{
+		breedImage.src = "dog.png"; 
+		raza.innerHTML =`La raza seleccionada es: ${prop}, elige una subraza.`;
+		console.log('array subraza pasado: ', pos);
+		console.log("raza pasada: ", prop);
+		for(let element = 0; element < pos.length; element++){
+			let breedP = document.createElement('p');
+			breed[0].appendChild(breedP);
+			breedP.innerHTML = pos[element]; // Comillas invertidas ``
+			console.log('subraza: ', pos[element]);
 			breedP.addEventListener("click", function(){
-				inserData('https://dog.ceo/api/breeds/list/all', 'subBreed', result.this);// prop no se si será válida
-			} );
-		}
-		if(breed_div != 'breed'){
-			breedP.innerHTML = pos.innerHTML;
-			console.log( 'PARAAAAAAAA  ' );
-			break;	
-		}
+				let xxxx = element;
+				dataImg(`https://dog.ceo/api/breed/${prop}/${pos[element]}/images/random`, breedImage);
+				 return xxxx;
+			});
+		} 
 	}
-}
-
- inserData('https://dog.ceo/api/breeds/list/all', 'breed', Object.keys(result));*/
-
-async function dataElement(url, breed_div, pos){
-	console.log('indexIndex pasado: ', pos);
-	let breed = document.getElementsByClassName(breed_div);
-    let data = await fetch(url);
-    let dataJson = await data.json();
-	let result = dataJson.message;
-	console.log('index pasado: ', pos);
-	/*Object.keys(result).forEach(function(prop, index){
-		if (pos == prop){
-			let resulta = Object.keys(result)[index];
-			console.log('resulta: ', resulta);
+	next.addEventListener("click", function(){
+		if(pos.length == 0){
+	   		dataImg(`https://dog.ceo/api/breed/${prop}/ /images/random`, breedImage)
 		}
-	})*/
-	let resulta = Object.keys(result)[pos];
-			console.log('resulta: ', resulta);
-	//console.log(`result.pos: `, dataJson.message.Object.keys(result)[pos]);
-	
-	
-	for(let prop = 0; prop < pos.length; prop++){
-		let breedP = document.createElement('p');
-		breed[0].appendChild(breedP);
-		breedP.innerHTML = result.pos[prop]; // Comillas invertidas ``
-		//breedP.addEventListener("click", dataElement);
-	}
+		else{
+			console.log('en el else nex: ', pos[xxxx]);  
+			dataImg(`https://dog.ceo/api/breed/${prop}/${xxxx} /images/random`, breedImage)
+		}
+	});
 }
-
-
 async function dataProp(url, breed_div){
 	let breed = document.getElementsByClassName(breed_div);
     let data = await fetch(url);
     let dataJson = await data.json();
 	let result = dataJson.message;
-	
-	Object.keys(result).forEach(function(prop, index){
+    
+	// con for in Funciona tb
+	/*for(let prop in result){
 		let breedP = document.createElement('p');
 		breed[0].appendChild(breedP);
 		breedP.innerHTML = prop; // Comillas invertidas ``
 		breedP.addEventListener("click", function(){
-			console.log('this.inner: ', this.innerHTML);
-			console.log('this_prop: ', prop);
-			console.log(index);
-			let indexIndex = Object.keys(result).prop;
-			console.log('indexIndex: ', index);
-			dataElement('https://dog.ceo/api/breeds/list/all', 'subBreed', indexIndex)
+			dataElement('subBreed', result[prop], prop)
+		});
+	}*/
+    const razas = Object.keys(result);
+    // tengo un array de razas
+    razas.forEach(function(raza,index){
+    	let breedP = document.createElement('p');
+        breed[0].appendChild(breedP);
+        breedP.innerHTML = raza;
+        breedP.addEventListener("click", function(){
+        	console.log(raza," . ",result[raza])
+			dataElement('subBreed', result[raza], raza)
 		});
 	})
 	
-	/*for(let prop = 0; prop < Object.keys(result).length; prop++){
-		console.log(Object.keys(result));
-		let breedP = document.createElement('p');
-		breed[0].appendChild(breedP);
-		breedP.innerHTML = Object.keys(result)[prop]; // Comillas invertidas ``
-		breedP.addEventListener("click", function(){
-			console.log('this.inner: ', this.innerHTML);
-			dataElement('https://dog.ceo/api/breeds/list/all', 'subBreed', this)
-		});
-	}*/
-}
+}	
 
 dataProp('https://dog.ceo/api/breeds/list/all', 'breed');
