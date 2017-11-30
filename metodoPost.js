061cfc1b72948d44f8e481eee983e96c3b99d630
Javascript <javascript@zPro6.local>
2017-11-30 21:44:26 +0100

    Post Put

--- metodoPost.js
+++ metodoPost.js
@@ -31,3 +31,31 @@ async function postEx(url, campaign){
 postEx('http://192.168.1.67:3000/campaign', campaign)
 
 // Igual cambiando POST por PUT 
+
+let campaign = {
+    name: "MODIFICACION PUT DONDE ESTAS....?",
+    title: "PUT MODIFICADO TITLE",
+};
+
+async function postEx(url, campaign){
+    try{
+        const headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        };
+        const data = await fetch(url, {
+            headers,
+            method: 'PUT',
+            body: JSON.stringify(campaign)
+        });
+        const objJson = await data.json();
+        console.log(objJson);
+        //console.log(data);
+    }catch(error){
+        console.log('error');
+    }
+}
+
+postEx('http://192.168.1.67:3000/campaign/5a2066b2c49f6c47dbbfb80a ', campaign)
+
+// Igual cambiando POST por GET 
